c5959f1991e3dfbf72bf294e17e9f79f5b654757
Tolebi <tolebizaksybaj@gmail.com>
2020-11-27 03:52:55 +0600

    svg

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
 
-$(document).ready(function () {
-	$('.header__sliders').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 3,
-		speed: 300,
-		arrows: false,
-		centerMode: true,
-		variableWidth: true,
-		lazyLoad: 'ondemand',
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		autoplaySpeed: 2000,
-	});
-});
-
